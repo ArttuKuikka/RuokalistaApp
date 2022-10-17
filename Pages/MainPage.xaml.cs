7b074b252ac3be2e7edf6d11af8cb758faef2dae
Arttu Kuikka <arttu.juhani.kuikka@gmail.com>
2022-10-17 12:26:16 +0300

    proo mut ei async

--- Pages/MainPage.xaml.cs
+++ Pages/MainPage.xaml.cs
@@ -16,6 +16,10 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 
+        
+
+        
+
         var url = "https://ruokalista.arttukuikka.fi/api/v1/Ruokalista";
 
         var httpRequest = (HttpWebRequest)WebRequest.Create(url);
@@ -29,14 +33,45 @@ public partial class MainPage : ContentPage
 
        var ruoka = JsonConvert.DeserializeObject<Ruokalista>(result);
 
-        int day = (int)DateTime.Now.DayOfWeek;
-        if(day == 1)
-        {
-            MaanantaiHeader.TextColor = Color.FromArgb("FFA500");
-            MaanantaiRuoka.TextColor = Color.FromArgb("FFA500");
-        }
+        Header.Text = Header.Text.Replace("00", ruoka.WeekId.ToString());
 
-        //tee switch juttu
+        MaanantaiHeader.Text = MaanantaiHeader.Text.Replace("00.00", System.Globalization.ISOWeek.ToDateTime(ruoka.Year, ruoka.WeekId, DayOfWeek.Monday).ToString("dd.MM"));
+        TiistaiHeader.Text = TiistaiHeader.Text.Replace("00.00", System.Globalization.ISOWeek.ToDateTime(ruoka.Year, ruoka.WeekId, DayOfWeek.Tuesday).ToString("dd.MM"));
+        KeskiviikkoHeader.Text = KeskiviikkoHeader.Text.Replace("00.00", System.Globalization.ISOWeek.ToDateTime(ruoka.Year, ruoka.WeekId, DayOfWeek.Wednesday).ToString("dd.MM"));
+        TorstaiHeader.Text = TorstaiHeader.Text.Replace("00.00", System.Globalization.ISOWeek.ToDateTime(ruoka.Year, ruoka.WeekId, DayOfWeek.Thursday).ToString("dd.MM"));
+        PerjantaiHeader.Text = PerjantaiHeader.Text.Replace("00.00", System.Globalization.ISOWeek.ToDateTime(ruoka.Year, ruoka.WeekId, DayOfWeek.Friday).ToString("dd.MM"));
+
+        int day = (int)DateTime.Now.DayOfWeek;
+        
+
+
+        switch (day)
+        {
+            case 1:
+                MaanantaiHeader.TextColor = Color.FromArgb("FFA500");
+                MaanantaiRuoka.TextColor = Color.FromArgb("FFA500");
+                break;
+            case 2:
+                TiistaiHeader.TextColor = Color.FromArgb("FFA500");
+                TiistaiRuoka.TextColor = Color.FromArgb("FFA500");
+                break;
+            case 3:
+                KeskiviikkoHeader.TextColor = Color.FromArgb("FFA500");
+                KeskiviikkoRuoka.TextColor = Color.FromArgb("FFA500");
+                break;
+            case 4:
+                TorstaiHeader.TextColor = Color.FromArgb("FFA500");
+                TorstaiRuoka.TextColor = Color.FromArgb("FFA500");
+                break;
+            case 5:
+                PerjantaiHeader.TextColor = Color.FromArgb("FFA500");
+                PerjantaiRuoka.TextColor = Color.FromArgb("FFA500");
+                break;
+            default:
+                break;
+
+        }
+        
 
         MaanantaiRuoka.Text = ruoka.Maanantai;
         TiistaiRuoka.Text = ruoka.Tiistai;
