25f3625084f108748175ea842c9a08e09b3a5689
Arttu Kuikka <arttu.juhani.kuikka@gmail.com>
2022-10-17 11:31:49 +0300

    proo

--- Pages/MainPage.xaml.cs
+++ Pages/MainPage.xaml.cs
@@ -1,17 +1,52 @@
-﻿namespace RuokalistaApp;
+﻿
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using RuokalistaApp.Models;
+using System.Drawing;
+using Color = Microsoft.Maui.Graphics.Color;
+
+namespace RuokalistaApp;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	
 
 	public MainPage()
 	{
 		InitializeComponent();
-	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		
-	}
+        var url = "https://ruokalista.arttukuikka.fi/api/v1/Ruokalista";
+
+        var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+
+        var result = "";
+        var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+        using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+        {
+            result = streamReader.ReadToEnd();
+        }
+
+       var ruoka = JsonConvert.DeserializeObject<Ruokalista>(result);
+
+        int day = (int)DateTime.Now.DayOfWeek;
+        if(day == 1)
+        {
+            MaanantaiHeader.TextColor = Color.FromArgb("FFA500");
+            MaanantaiRuoka.TextColor = Color.FromArgb("FFA500");
+        }
+
+        //tee switch juttu
+
+        MaanantaiRuoka.Text = ruoka.Maanantai;
+        TiistaiRuoka.Text = ruoka.Tiistai;
+        KeskiviikkoRuoka.Text = ruoka.Keskiviikko;
+        TorstaiRuoka.Text = ruoka.Torstai;
+        PerjantaiRuoka.Text = ruoka.Perjantai;
+
+        
+    }
+
+	
 }
 
