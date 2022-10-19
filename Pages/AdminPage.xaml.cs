90c6d3aec27b8d7dde768986bccaf89d4bbfead8
Arttu Kuikka <arttu.juhani.kuikka@gmail.com>
2022-10-19 21:22:16 +0300

    AdminPage cs parannuksia

--- Pages/AdminPage.xaml.cs
+++ Pages/AdminPage.xaml.cs
@@ -1,3 +1,5 @@
+using Java.Time.Temporal;
+
 namespace RuokalistaApp.Pages;
 
 public partial class AdminPage : ContentPage
@@ -5,5 +7,33 @@ public partial class AdminPage : ContentPage
 	public AdminPage()
 	{
 		InitializeComponent();
+		dothething();
+
+	}
+
+	public async void dothething()
+	{
+		await Load();
+	}
+
+	public async Task Load()
+	{
+		RuokaView.Children.Clear();
+
+		var viikko = 52;
+		var vuosi = 2022;
+
+		for(int i = 0; i < 15; i++)
+		{
+            var Ruoka = new HorizontalStackLayout() { Padding = new Thickness(0, 5), Spacing = 15 };
+            var tekstit = new VerticalStackLayout();
+            tekstit.Children.Add(new Label() { FontSize = 20, Text = "Viikko " + viikko.ToString() });
+            tekstit.Children.Add(new Label() { Text = vuosi.ToString() });
+            Ruoka.Children.Add(tekstit);
+            Ruoka.Children.Add(new Button() { Text = "Muokkaa" });
+            Ruoka.Children.Add(new Button() { Text = "Tiedot" });
+            RuokaView.Children.Add(Ruoka);
+        }
+
 	}
 }
