57c3c9304e26a5ebe94b05427389af4a6cf5d64d
Arttu Kuikka <arttu.juhani.kuikka@gmail.com>
2022-10-20 00:08:56 +0300

    proo

--- Pages/AdminPage.xaml.cs
+++ Pages/AdminPage.xaml.cs
@@ -1,5 +1,8 @@
 
 
+using Newtonsoft.Json;
+using RuokalistaApp.Models;
+
 namespace RuokalistaApp.Pages;
 
 public partial class AdminPage : ContentPage
@@ -18,24 +21,50 @@ public partial class AdminPage : ContentPage
 
 	public async Task Load()
 	{
-
-		await Task.Delay(10000);
 		RuokaView.Children.Clear();
 
-		var viikko = 52;
-		var vuosi = 2022;
+        var result = "";
+        using (var client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", await SecureStorage.Default.GetAsync("token"));
+            client.BaseAddress = new Uri("https://ruokalista.arttukuikka.fi/");
 
-		for(int i = 0; i < 15; i++)
-		{
+            //GET Method
+            HttpResponseMessage response = await client.GetAsync("api/v1/Ruokalista/Get/20");
+            if (response.IsSuccessStatusCode)
+            {
+                result = await response.Content.ReadAsStringAsync();
+
+            }
+            else
+            {
+                if(response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                {
+                    await DisplayAlert("Virhe " + response.StatusCode.ToString(), "Kirjautumisesi on vanhentunut, kirjaudu uudelleen sisään asetukset välilehdeltä", "ok");
+                    Preferences.Default.Set("IsAdmin", false);
+                }
+                else
+                {
+                    await DisplayAlert("Virhe " + response.StatusCode.ToString(), "virhe ladatessa sisältöä", "ok");
+                }
+            }
+        }
+
+        var ruokaarray = JsonConvert.DeserializeObject<Ruokalista[]>(result);
+
+        foreach(var ruoka in ruokaarray)
+        {
             var Ruoka = new HorizontalStackLayout() { Padding = new Thickness(0, 5), Spacing = 15 };
             var tekstit = new VerticalStackLayout();
-            tekstit.Children.Add(new Label() { FontSize = 20, Text = "Viikko " + viikko.ToString() });
-            tekstit.Children.Add(new Label() { Text = vuosi.ToString() });
+            tekstit.Children.Add(new Label() { FontSize = 20, Text = "Viikko " + ruoka.WeekId.ToString() });
+            tekstit.Children.Add(new Label() { Text = ruoka.Year.ToString() });
             Ruoka.Children.Add(tekstit);
             Ruoka.Children.Add(new Button() { Text = "Muokkaa" });
             Ruoka.Children.Add(new Button() { Text = "Tiedot" });
             RuokaView.Children.Add(Ruoka);
         }
+
+        
 
 	}
 
--- Pages/CreateNewPage.xaml.cs
+++ Pages/CreateNewPage.xaml.cs
@@ -1,3 +1,9 @@
+using Newtonsoft.Json;
+using RuokalistaApp.Models;
+using static Android.Content.ClipData;
+using System.Net;
+using System.Text;
+
 namespace RuokalistaApp.Pages;
 
 public partial class CreateNewPage : ContentPage
@@ -11,14 +17,97 @@ public partial class CreateNewPage : ContentPage
 
     }
 
-	private void Kuvatunnistus_Clicked(object sender, EventArgs e)
+	private async void Kuvatunnistus_Clicked(object sender, EventArgs e)
 	{
+        if (MediaPicker.Default.IsCaptureSupported)
+        {
+            FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
 
-	}
+            if (photo != null)
+            {
+                // save the file into local storage
+                string localFilePath = Path.Combine(FileSystem.CacheDirectory, "ruokalista.jpg");
 
-    private void Valmis_Clicked(object sender, EventArgs e)
+                using Stream sourceStream = await photo.OpenReadAsync();
+                using FileStream localFileStream = File.OpenWrite(localFilePath);
+
+                await sourceStream.CopyToAsync(localFileStream);
+            }
+        }
+    }
+
+    private async void Valmis_Clicked(object sender, EventArgs e)
     {
+		VaroitusLabel.Text = "";
 
+		var vuosioikein = int.TryParse(VuosiEntry.Text, out int vuosiparsed);
+		var viikkooikein = int.TryParse(ViikkoEntry.Text, out int viikkoparsed);
+		if (!vuosioikein || vuosiparsed < 2000 || vuosiparsed > 2050)
+		{
+			VaroitusLabel.Text += "\nVuosi v‰‰rin";
+			return;
+		}
+		if (!viikkooikein)
+		{
+			VaroitusLabel.Text += "\nViikko v‰‰rin";
+			return;
+		}
+		if(MaanantaiEntry.Text == null || TiistaiEntry.Text == null || KeskiviikkoEntry.Text == null || TorstaiEntry.Text == null || PerjantaiEntry.Text == null)
+		{
+            VaroitusLabel.Text += "\nTyhji‰ kentti‰";
+            return;
+        }
+		if(MaanantaiEntry.Text.Length < 1 || TiistaiEntry.Text.Length < 1 || KeskiviikkoEntry.Text.Length < 1 || TorstaiEntry.Text.Length < 1 || PerjantaiEntry.Text.Length < 1)
+		{
+            VaroitusLabel.Text += "\nTyhji‰ kentti‰";
+			return;
+        }
+
+		var r = new Ruokalista()
+		{
+			WeekId = viikkoparsed,
+			Year = vuosiparsed,
+			Maanantai = MaanantaiEntry.Text,
+			Tiistai = TiistaiEntry.Text,
+			Keskiviikko = KeskiviikkoEntry.Text,
+			Torstai = TorstaiEntry.Text,
+			Perjantai = PerjantaiEntry.Text
+        };
+        var json = JsonConvert.SerializeObject(r);
+
+        HttpContent c = new StringContent(json, Encoding.UTF8, "application/json");
+
+        
+        using (var client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", await SecureStorage.Default.GetAsync("token"));
+            HttpRequestMessage request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://ruokalista.arttukuikka.fi/api/v1/Ruokalista/Create"),
+                Content = c,
+                
+            };
+
+            HttpResponseMessage result = await client.SendAsync(request);
+            if (result.IsSuccessStatusCode)
+            {
+                await Shell.Current.GoToAsync("..");
+            }
+            else
+            {
+                if (result.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    await DisplayAlert("Virhe", "Virheellinen k‰ytt‰j‰tunnus tai salasana, kirjaudu uudelleen sis‰‰n", "Ok");
+                    return;
+                }
+                else
+                {
+                    await DisplayAlert("Virhe " + result.StatusCode.ToString(), "Virhe yhdist‰ess‰ palvelimeen tai virhe palvelimella", "Ok");
+                    return;
+                }
+            }
+        }
     }
 
     private async void Peruuta_Clicked(object sender, EventArgs e)
