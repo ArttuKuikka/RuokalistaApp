c826f3f64cb7c7bbdd1090386769037fdf2a1ab7
Arttu Kuikka <arttu.juhani.kuikka@gmail.com>
2022-10-23 19:25:33 +0300

    Ilmoitukset

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -7,4 +7,26 @@ namespace RuokalistaApp;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    internal static readonly string Channel_ID = "Main";
+    internal static readonly int NotificationID = 101;
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        CreateNotificationChannel();
+
+        
+    }
+
+    private void CreateNotificationChannel()
+    {
+        if(OperatingSystem.IsOSPlatformVersionAtLeast("android", 26))
+        {
+            var channel = new NotificationChannel(Channel_ID, "kaikki Ilmoitukset", NotificationImportance.Default);
+
+            var notificationmanager = (NotificationManager)GetSystemService(Android.Content.Context.NotificationService);
+            notificationmanager.CreateNotificationChannel(channel);
+        }
+        
+    }
 }
---
+++ Platforms/Android/Services/FirebaseService.cs
@@ -0,0 +1,55 @@
+﻿using Android.App;
+using AndroidX.Core.App;
+using Firebase.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuokalistaApp.Platforms.Android.Services
+{
+    [Service(Exported = true)]
+    [IntentFilter(new[] {"com.google.firebase.MESSAGING_EVENT"})]
+    public class FirebaseService : FirebaseMessagingService
+    {
+        public FirebaseService()
+        {
+
+        }
+
+        public override void OnNewToken(string token)
+        {
+            base.OnNewToken(token);
+            if (Preferences.ContainsKey("DeviceToken"))
+            {
+                Preferences.Remove("DeviceToken");
+            }
+            Preferences.Set("DeviceToken", token);
+        }
+
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            base.OnMessageReceived(message);
+            var notification = message.GetNotification();
+
+            SendNotification(notification.Body, notification.Title, message.Data);
+        }
+
+        private void SendNotification(string messageBody, string title, IDictionary<string, string> data)
+        {
+
+            var notificationbuilder = new NotificationCompat.Builder(this, MainActivity.Channel_ID)
+                .SetContentTitle(title)
+                .SetContentTitle(messageBody)
+                .SetChannelId(MainActivity.Channel_ID)
+                .SetSmallIcon(0);
+
+            var notificationmanager = NotificationManager.FromContext(this);
+            notificationmanager.Notify(MainActivity.NotificationID, notificationbuilder.Build());
+        }
+
+    }
+
+    
+}
