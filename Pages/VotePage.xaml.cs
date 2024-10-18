b25ea4eda82ad2bb5e73e9d7e3e7203b39554955
Arttu Kuikka <75498768+ArttuKuikka@users.noreply.github.com>
2024-10-18 21:31:29 +0300

    vote dev check

--- Pages/VotePage.xaml.cs
+++ Pages/VotePage.xaml.cs
@@ -5,6 +5,16 @@ public partial class VotePage : ContentPage
 	public VotePage()
 	{
 		InitializeComponent();
+
+		var isDev = Preferences.Default.Get("IsDeveloper", false);
+		if (isDev)
+		{
+			webview1.Source = "https://ruokalistadev.arttukuikka.fi/Aanestys/Tulokset?isApp=true";
+		}
+		else
+		{
+			webview1.Source = "https://ruokalista.arttukuikka.fi/Aanestys/Tulokset?isApp=true";
+		}
 	}
 
     private void RefreshView_Refreshing(object sender, EventArgs e)
